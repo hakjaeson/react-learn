885f9f5928f819f482503d03e2800454fe83ac7f
devgreact <dev.greact@gmail.com>
2023-11-21 16:38:59 +0900

    docs : state 의 이해(reRendering 의 이해)

--- README.md
+++ README.md
@@ -1,73 +1,153 @@
 # React 공부하기
 
-## 4. 컴포넌트 js의 이해
+## 5. 컴포넌트 갱신(새로고침)의 이해
 
-### 4.5. 이벤트(onSubmit)의 이해
+### 5.1. state 의 이해 : Re-Rendering
 
-- 주의사항 (form 태그 안의 button 주의하세요.)
+- 화면의 데이터를 출력한다는 것은 리랜더링 진행하는 것을 의미
+- 직접 코드를 통해 리랜더링을 진행해야 한다.
 
-  ```html
-  <button>버튼</button>
-  ```
-
-  ```html
-  <!-- 이러시면 곤란합니다. submit 실행됩니다. -->
-  <form>
-    <!-- 자동으로 type 이 submit 셋팅됩니다. -->
-    <!-- form 의 데이터를 전송하겠다는 의미 -->
-    <button>버튼</button>
-  </form>
-  ```
-
-  ```html
-  <form>
-    <!-- form 안에서 type 을 명시한 경우  -->
-    <button type="button">버튼</button>
-  </form>
-  ```
-
-- onSubmit 적용하기
-
-  ```js
-  <form action="/path" method="get" name="join" onSubmit={function (event) {}}>
-    ...
-  </form>
-  ```
-
-  ```js
-  <form action="/path" method="get" name="join" onSubmit={event => {}}>
-    ...
-  </form>
-  ```
-
-  ```js
-   // 데이터 전송 처리 함수
-  const handleSubmitJoinForm = event => {};
-  ....
-  <form
-        action="/path"
-        method="get"
-        name="join"
-        onSubmit={event => handleSubmitJoinForm(event)}
-      >
-  ....
-  </form>
-  ```
-
-- 반드시 form 처리를 해야 합니다.
-
-  ```js
-  // 데이터 전송 처리 함수
-  const handleSubmitJoinForm = event => {
-    // 기본 동작 막기
-    event.preventDefault();
-
-    console.log(event);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.target.name);
-    console.log(event.target.action);
-    console.log(event.target.method);
-    console.log(event.target.onSubmit);
+```js
+const App = () => {
+  // 일반 변수
+  let count = 0;
+  const handleClickAdd = () => {
+    count = count + 1;
+    console.log(count);
+    // 리랜더링: 화면 내용 을 갱신
+    const pTag = document.querySelector("p");
+    pTag.innerHTML = "현재 : " + count;
   };
-  ```
+  const handleClickSub = () => {
+    count = count - 1;
+    console.log(count);
+    // 리랜더링: 화면 내용 을 갱신
+    const pTag = document.querySelector("p");
+    pTag.innerHTML = "현재 : " + count;
+  };
+  return (
+    <div>
+      <p>현재 : {count}</p>
+      <button
+        onClick={() => {
+          handleClickAdd();
+        }}
+      >
+        증가
+      </button>
+      <button
+        onClick={() => {
+          handleClickSub();
+        }}
+      >
+        감소
+      </button>
+    </div>
+  );
+};
+
+export default App;
+```
+
+```js
+import { useState } from "react";
+
+const App = () => {
+  // 상태 변수 : 리랜더링이 되요.
+  // 상태 변수 문법
+  // [geeter, setter] = state(초기값)
+  const [count, setCount] = useState(0);
+  const handleClickAdd = () => {
+    const now = count + 1;
+    setCount(now);
+  };
+  const handleClickSub = () => {
+    const now = count - 1;
+    setCount(now);
+  };
+  return (
+    <div>
+      <p>현재 : {count}</p>
+      <button
+        onClick={() => {
+          handleClickAdd();
+        }}
+      >
+        증가
+      </button>
+      <button
+        onClick={() => {
+          handleClickSub();
+        }}
+      >
+        감소
+      </button>
+    </div>
+  );
+};
+
+export default App;
+```
+
+```js
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  // 사용자 아이디 보관 및 리랜더링
+  const [userId, setUserId] = useState("");
+
+  // 사용자 패스워드 보관 및 리랜더링
+  const [userPass, setUserPass] = useState("");
+
+  // 데이터 전송
+  const handleSubmit = e => {
+    // 반드시 기본 동작을 막아준다.
+    e.preventDefault();
+    console.log(userId, userPass);
+    if (userId === "") {
+      alert("아이디를 입력하세요.");
+      return;
+    }
+    if (userPass === "") {
+      alert("패스워드를 입력하세요.");
+      return;
+    }
+    alert("데이터가 등록되었습니다.");
+    setUserId("");
+    setUserPass("");
+  };
+
+  // 폼 입력 처리
+  const handleChange = e => {
+    // console.log(e.target);
+    if (e.target.name === "user") {
+      setUserId(e.target.value);
+    }
+    if (e.target.name === "pw") {
+      setUserPass(e.target.value);
+    }
+  };
+  return (
+    <div>
+      <form action="path" method="get" onSubmit={e => handleSubmit(e)}>
+        <input
+          type="text"
+          name="user"
+          value={userId}
+          onChange={e => handleChange(e)}
+        />
+        <input
+          type="password"
+          name="pw"
+          value={userPass}
+          onChange={e => handleChange(e)}
+        />
+        <button type="submit">완료</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+```
--- src/App.js
+++ src/App.js
@@ -1,171 +1,57 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  // 입력값 제어 함수
-  const handleChangeJoinForm = event => {
-    // console.log(event.target);
-    // console.log(event.type);
-    console.log(event.target.name);
-    console.log(event.target.value);
-    // console.log(event.target.type);
-    // console.log(event.target.id);
+  // 사용자 아이디 보관 및 리랜더링
+  const [userId, setUserId] = useState("");
+
+  // 사용자 패스워드 보관 및 리랜더링
+  const [userPass, setUserPass] = useState("");
+
+  // 데이터 전송
+  const handleSubmit = e => {
+    // 반드시 기본 동작을 막아준다.
+    e.preventDefault();
+    console.log(userId, userPass);
+    if (userId === "") {
+      alert("아이디를 입력하세요.");
+      return;
+    }
+    if (userPass === "") {
+      alert("패스워드를 입력하세요.");
+      return;
+    }
+    alert("데이터가 등록되었습니다.");
+    setUserId("");
+    setUserPass("");
   };
 
-  // 데이터 전송 처리 함수
-  const handleSubmitJoinForm = event => {
-    // 기본 동작 막기
-    event.preventDefault();
-    console.log(event);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.target.name);
-    console.log(event.target.action);
-    console.log(event.target.method);
-    console.log(event.target.onSubmit);
+  // 폼 입력 처리
+  const handleChange = e => {
+    // console.log(e.target);
+    if (e.target.name === "user") {
+      setUserId(e.target.value);
+    }
+    if (e.target.name === "pw") {
+      setUserPass(e.target.value);
+    }
   };
-
   return (
     <div>
-      <form
-        action="/path"
-        method="get"
-        name="join"
-        onSubmit={event => handleSubmitJoinForm(event)}
-      >
-        <fieldset>
-          <legend>회원가입</legend>
-          <label htmlFor="user">아이디</label>
-          <input
-            type="text"
-            name="user"
-            id="user"
-            placeholder="아이디 입력해주세요."
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <input type="button" name="idcheck" value="중복확인" />
-          <br />
-          <label htmlFor="pw">비밀번호</label>
-          <input
-            type="password"
-            name="pw"
-            id="pw"
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <br />
-        </fieldset>
-
-        <fieldset>
-          <legend>정보입력</legend>
-          <label htmlFor="age">나이</label>
-          <input
-            type="number"
-            name="age"
-            id="age"
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <br />
-          <label htmlFor="gm">남성</label>
-          <input
-            type="radio"
-            name="gender"
-            value="m"
-            id="gm"
-            checked={true}
-            onClick={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <label htmlFor="gf">여성</label>
-          <input
-            type="radio"
-            name="gender"
-            value="f"
-            id="gf"
-            onClick={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-
-          <br />
-          <label htmlFor="js">JS</label>
-          <input
-            type="checkbox"
-            name="js"
-            id="js"
-            onClick={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <label htmlFor="css">CSS</label>
-          <input
-            type="checkbox"
-            name="css"
-            id="css"
-            onClick={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <label htmlFor="html">HTML</label>
-          <input
-            type="checkbox"
-            name="html"
-            id="html"
-            onClick={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <br />
-          <label htmlFor="level">성적등급</label>
-          <select
-            name="level"
-            id="level"
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          >
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-          </select>
-          <br />
-          <label htmlFor="file">파일첨부</label>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          />
-          <br />
-          <label htmlFor="etc">기타사항</label>
-          <textarea
-            name="etc"
-            id="etc"
-            onChange={event => {
-              handleChangeJoinForm(event);
-            }}
-          ></textarea>
-        </fieldset>
-
-        <fieldset>
-          <legend>버튼들</legend>
-          <input type="reset" value="다시작성" />
-          <input type="submit" value="작성완료" />
-
-          {/* form 태그 안에 작성시 */}
-          <button type="submit"> type 이 없는 경우 버튼 작성 완료</button>
-          <button type="button"> type 이 있는 경우 버튼 작성 완료</button>
-
-          <input type="image" src="images/a.jpg" />
-        </fieldset>
+      <form action="path" method="get" onSubmit={e => handleSubmit(e)}>
+        <input
+          type="text"
+          name="user"
+          value={userId}
+          onChange={e => handleChange(e)}
+        />
+        <input
+          type="password"
+          name="pw"
+          value={userPass}
+          onChange={e => handleChange(e)}
+        />
+        <button type="submit">완료</button>
       </form>
     </div>
   );
